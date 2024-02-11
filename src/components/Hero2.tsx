1dd35060edf51d089ee702b1a2867c989a74cd73
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-02-11 11:44:39 -0500

    Renamed hero css to reusable names.

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -5,6 +5,7 @@ import langingImg from "../assets/hero-image.png";
 const Hero2 = () => {
   return (
     <div className="container hero">
+      {/* unused: hero-text */}
       <div className="hero-text">
         <h1 className="hero-heading">
           Curated Collection of Everyday Essentials.
@@ -14,13 +15,13 @@ const Hero2 = () => {
           destination for curated styles, top designers, and exceptional
           service.
         </p>
-        <div className="hero-buttons">
+        <div className="btns-flexed">
           <button className="btn btn-primary">Shop Now</button>
           <button className="btn btn-primary">Ad to Cart</button>
         </div>
         <div>
           <p>Available on</p>
-          <div className="hero-icons">
+          <div className="icons-flexed">
             <a href="#" target="_blank">
               <img src={amazon} alt="" className="logo-icon" />
             </a>
